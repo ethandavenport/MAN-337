ed6c611c97bf12c3ae32f12e26c73fbccdc9fb59
ethandavenport <139188296+ethandavenport@users.noreply.github.com>
2023-11-25 12:38:51 -0600

    Update Final Project.R

--- Final Project.R
+++ Final Project.R
@@ -14,85 +14,184 @@ epm16 = read.csv("C:/Users/ethan/OneDrive/Fall 2023/MAN 337/EPM_2016.csv")
 epm15 = read.csv("C:/Users/ethan/OneDrive/Fall 2023/MAN 337/EPM_2015.csv")
 epm14 = read.csv("C:/Users/ethan/OneDrive/Fall 2023/MAN 337/EPM_2014.csv")
 
-epm = rbind(epm23, epm22, epm21, epm20, epm19, epm18, epm17, epm16, epm15, epm14)
-  
-#epm = epm_all %>%
-  #pivot_wider(names_from = season,
-              #values_from = team) %>%
-  #select(-gp, -min, -mpg, -oepm, -depm, -epm, -ewins) %>%
-  #group_by(name) %>%
-  #summarize('2023' = 'hello')
-
 # widen the data sets to show each player's path over their career
-team_paths = epm %>%
-  select(-gp, -min, -mpg, -oepm, -depm, -epm, -ewins) %>%
-  spread(key = season, value = team)
+epm1 = rbind(epm22, epm21)
+tp1 = epm1 %>% select(season, nba_id, name, team) %>% spread(key = season, value = team)
+ep1 = epm1 %>% select(season, nba_id, name, epm) %>% spread(key = season, value = epm)
+op1 = epm1 %>% select(season, nba_id, name, oepm) %>% spread(key = season, value = oepm)
+dp1 = epm1 %>% select(season, nba_id, name, depm) %>% spread(key = season, value = depm)
 
-epm_paths = epm %>%
-  select(-gp, -min, -mpg, -oepm, -depm, -team, -ewins) %>%
-  spread(key = season, value = epm)
+# filter data sets to only players that changed teams over the offseason
+tp1 = tp1 %>% filter(`2021` != `2022` & !is.na(`2021`) & !is.na(`2022`))
+ep1 = ep1 %>% filter(name %in% tp1$name)
+op1 = op1 %>% filter(name %in% tp1$name)
+dp1 = dp1 %>% filter(name %in% tp1$name)
+  
+# combine the path data sets, naming columns appropriately
+paths1 = tp1 %>%
+  left_join(ep1, by = c("nba_id", "name"), suffix = c("", ".e")) %>%
+  left_join(op1, by = c("nba_id", "name"), suffix = c("", ".o")) %>%
+  left_join(dp1, by = c("nba_id", "name"), suffix = c("", ".d"))
 
-oepm_paths = epm %>%
-  select(-gp, -min, -mpg, -epm, -depm, -team, -ewins) %>%
-  spread(key = season, value = oepm)
+paths1 = paths1 %>% mutate(old.yr = 2021, new.yr = 2022) %>%
+  rename(oldtm = `2021`, newtm = `2022`, old.epm = `2021.e`, new.epm = `2022.e`,
+         old.oepm = `2021.o`, new.oepm = `2022.o`, old.depm = `2021.d`, new.depm = `2022.d`)
 
-depm_paths = epm %>%
-  select(-gp, -min, -mpg, -oepm, -epm, -team, -ewins) %>%
-  spread(key = season, value = depm)
+# repeat this process for the next set of years
+epm2 = rbind(epm21, epm20)
+tp2 = epm2 %>% select(season, nba_id, name, team) %>% spread(key = season, value = team)
+ep2 = epm2 %>% select(season, nba_id, name, epm) %>% spread(key = season, value = epm)
+op2 = epm2 %>% select(season, nba_id, name, oepm) %>% spread(key = season, value = oepm)
+dp2 = epm2 %>% select(season, nba_id, name, depm) %>% spread(key = season, value = depm)
 
-# filter these data sets to players who switched teams at some point
-team_paths = team_paths %>%
-  filter(`2014` != `2015` & !is.na(`2014`) & !is.na(`2015`) |
-           `2015` != `2016` & !is.na(`2015`) & !is.na(`2016`) |
-           `2016` != `2017` & !is.na(`2016`) & !is.na(`2017`) |
-           `2017` != `2018` & !is.na(`2017`) & !is.na(`2018`) |
-           `2018` != `2019` & !is.na(`2018`) & !is.na(`2019`) |
-           `2019` != `2020` & !is.na(`2019`) & !is.na(`2020`) |
-           `2020` != `2021` & !is.na(`2020`) & !is.na(`2021`) |
-           `2021` != `2022` & !is.na(`2021`) & !is.na(`2022`) |
-           `2022` != `2023` & !is.na(`2022`) & !is.na(`2023`))
+tp2 = tp2 %>% filter(`2020` != `2021` & !is.na(`2020`) & !is.na(`2021`))
+ep2 = ep2 %>% filter(name %in% tp2$name)
+op2 = op2 %>% filter(name %in% tp2$name)
+dp2 = dp2 %>% filter(name %in% tp2$name)
 
-epm_paths = epm_paths %>%
-  filter(name %in% team_paths$name)
+paths2 = tp2 %>%
+  left_join(ep2, by = c("nba_id", "name"), suffix = c("", ".e")) %>%
+  left_join(op2, by = c("nba_id", "name"), suffix = c("", ".o")) %>%
+  left_join(dp2, by = c("nba_id", "name"), suffix = c("", ".d"))
 
-oepm_paths = oepm_paths %>%
-  filter(name %in% team_paths$name)
+paths2 = paths2 %>% mutate(old.yr = 2020, new.yr = 2021) %>%
+  rename(oldtm = `2020`, newtm = `2021`, old.epm = `2020.e`, new.epm = `2021.e`,
+         old.oepm = `2020.o`, new.oepm = `2021.o`, old.depm = `2020.d`, new.depm = `2021.d`)
 
-depm_paths = depm_paths %>%
-  filter(name %in% team_paths$name)
+# repeat this process for the next set of years
+epm3 = rbind(epm20, epm19)
+tp3 = epm3 %>% select(season, nba_id, name, team) %>% spread(key = season, value = team)
+ep3 = epm3 %>% select(season, nba_id, name, epm) %>% spread(key = season, value = epm)
+op3 = epm3 %>% select(season, nba_id, name, oepm) %>% spread(key = season, value = oepm)
+dp3 = epm3 %>% select(season, nba_id, name, depm) %>% spread(key = season, value = depm)
 
-# write into CSVs for manipulation in python
-write.csv(team_paths, "C:/Users/ethan/OneDrive/Fall 2023/MAN 337/team_paths.csv")
-write.csv(epm_paths, "C:/Users/ethan/OneDrive/Fall 2023/MAN 337/epm_paths.csv")
-write.csv(oepm_paths, "C:/Users/ethan/OneDrive/Fall 2023/MAN 337/oepm_paths.csv")
-write.csv(depm_paths, "C:/Users/ethan/OneDrive/Fall 2023/MAN 337/depm_paths.csv")
+tp3 = tp3 %>% filter(`2019` != `2020` & !is.na(`2019`) & !is.na(`2020`))
+ep3 = ep3 %>% filter(name %in% tp3$name)
+op3 = op3 %>% filter(name %in% tp3$name)
+dp3 = dp3 %>% filter(name %in% tp3$name)
 
-# turn teams not in a relevant year into NA
-team_paths = team_paths %>%
-  mutate(tm14 = ifelse(`2014` == `2015`, NA, `2014`),
-         tm15 = ifelse(`2014` == `2015` & `2015` == `2016`, NA, `2015`),
-         tm16 = ifelse(`2015` == `2016` & `2016` == `2017`, NA, `2016`),
-         tm17 = ifelse(`2016` == `2017` & `2017` == `2018`, NA, `2017`),
-         tm18 = ifelse(`2017` == `2018` & `2018` == `2019`, NA, `2018`),
-         tm19 = ifelse(`2018` == `2019` & `2019` == `2020`, NA, `2019`),
-         tm20 = ifelse(`2019` == `2020` & `2020` == `2021`, NA, `2020`),
-         tm21 = ifelse(`2020` == `2021` & `2021` == `2022`, NA, `2021`),
-         tm22 = ifelse(`2021` == `2022` & `2022` == `2023`, NA, `2022`),
-         tm23 = ifelse(`2022` == `2023`, NA, `2023`)) %>%
-  select(-`2014`, -`2015`, -`2016`, -`2017`, -`2018`, -`2019`, -`2020`, -`2021`, -`2022`, -`2023`)
+paths3 = tp3 %>%
+  left_join(ep3, by = c("nba_id", "name"), suffix = c("", ".e")) %>%
+  left_join(op3, by = c("nba_id", "name"), suffix = c("", ".o")) %>%
+  left_join(dp3, by = c("nba_id", "name"), suffix = c("", ".d"))
 
-# combine files
-paths = team_paths %>%
-  left_join(epm_paths, by = c("nba_id", "name"), suffix = c("", ".e")) %>%
-  left_join(oepm_paths, by = c("nba_id", "name"), suffix = c("", ".o")) %>%
-  left_join(depm_paths, by = c("nba_id", "name"), suffix = c("", ".d"))
+paths3 = paths3 %>% mutate(old.yr = 2019, new.yr = 2020) %>%
+  rename(oldtm = `2019`, newtm = `2020`, old.epm = `2019.e`, new.epm = `2020.e`,
+         old.oepm = `2019.o`, new.oepm = `2020.o`, old.depm = `2019.d`, new.depm = `2020.d`)
 
+# repeat this process for the next set of years
+epm4 = rbind(epm19, epm18)
+tp4 = epm4 %>% select(season, nba_id, name, team) %>% spread(key = season, value = team)
+ep4 = epm4 %>% select(season, nba_id, name, epm) %>% spread(key = season, value = epm)
+op4 = epm4 %>% select(season, nba_id, name, oepm) %>% spread(key = season, value = oepm)
+dp4 = epm4 %>% select(season, nba_id, name, depm) %>% spread(key = season, value = depm)
 
+tp4 = tp4 %>% filter(`2018` != `2019` & !is.na(`2018`) & !is.na(`2019`))
+ep4 = ep4 %>% filter(name %in% tp4$name)
+op4 = op4 %>% filter(name %in% tp4$name)
+dp4 = dp4 %>% filter(name %in% tp4$name)
 
-# ideal final data set columns: player, old team, various EPMs, new team, various EPMs
+paths4 = tp4 %>%
+  left_join(ep4, by = c("nba_id", "name"), suffix = c("", ".e")) %>%
+  left_join(op4, by = c("nba_id", "name"), suffix = c("", ".o")) %>%
+  left_join(dp4, by = c("nba_id", "name"), suffix = c("", ".d"))
 
-# adjust for age?
+paths4 = paths4 %>% mutate(old.yr = 2018, new.yr = 2019) %>%
+  rename(oldtm = `2018`, newtm = `2019`, old.epm = `2018.e`, new.epm = `2019.e`,
+         old.oepm = `2018.o`, new.oepm = `2019.o`, old.depm = `2018.d`, new.depm = `2019.d`)
+
+# repeat this process for the next set of years
+epm5 = rbind(epm18, epm17)
+tp5 = epm5 %>% select(season, nba_id, name, team) %>% spread(key = season, value = team)
+ep5 = epm5 %>% select(season, nba_id, name, epm) %>% spread(key = season, value = epm)
+op5 = epm5 %>% select(season, nba_id, name, oepm) %>% spread(key = season, value = oepm)
+dp5 = epm5 %>% select(season, nba_id, name, depm) %>% spread(key = season, value = depm)
+
+tp5 = tp5 %>% filter(`2017` != `2018` & !is.na(`2017`) & !is.na(`2018`))
+ep5 = ep5 %>% filter(name %in% tp5$name)
+op5 = op5 %>% filter(name %in% tp5$name)
+dp5 = dp5 %>% filter(name %in% tp5$name)
+
+paths5 = tp5 %>%
+  left_join(ep5, by = c("nba_id", "name"), suffix = c("", ".e")) %>%
+  left_join(op5, by = c("nba_id", "name"), suffix = c("", ".o")) %>%
+  left_join(dp5, by = c("nba_id", "name"), suffix = c("", ".d"))
+
+paths5 = paths5 %>% mutate(old.yr = 2017, new.yr = 2018) %>%
+  rename(oldtm = `2017`, newtm = `2018`, old.epm = `2017.e`, new.epm = `2018.e`,
+         old.oepm = `2017.o`, new.oepm = `2018.o`, old.depm = `2017.d`, new.depm = `2018.d`)
+
+# repeat this process for the next set of years
+epm6 = rbind(epm17, epm16)
+tp6 = epm6 %>% select(season, nba_id, name, team) %>% spread(key = season, value = team)
+ep6 = epm6 %>% select(season, nba_id, name, epm) %>% spread(key = season, value = epm)
+op6 = epm6 %>% select(season, nba_id, name, oepm) %>% spread(key = season, value = oepm)
+dp6 = epm6 %>% select(season, nba_id, name, depm) %>% spread(key = season, value = depm)
+
+tp6 = tp6 %>% filter(`2016` != `2017` & !is.na(`2016`) & !is.na(`2017`))
+ep6 = ep6 %>% filter(name %in% tp6$name)
+op6 = op6 %>% filter(name %in% tp6$name)
+dp6 = dp6 %>% filter(name %in% tp6$name)
+
+paths6 = tp6 %>%
+  left_join(ep6, by = c("nba_id", "name"), suffix = c("", ".e")) %>%
+  left_join(op6, by = c("nba_id", "name"), suffix = c("", ".o")) %>%
+  left_join(dp6, by = c("nba_id", "name"), suffix = c("", ".d"))
+
+paths6 = paths6 %>% mutate(old.yr = 2016, new.yr = 2017) %>%
+  rename(oldtm = `2016`, newtm = `2017`, old.epm = `2016.e`, new.epm = `2017.e`,
+         old.oepm = `2016.o`, new.oepm = `2017.o`, old.depm = `2016.d`, new.depm = `2017.d`)
+
+# repeat this process for the next set of years
+epm7 = rbind(epm16, epm15)
+tp7 = epm7 %>% select(season, nba_id, name, team) %>% spread(key = season, value = team)
+ep7 = epm7 %>% select(season, nba_id, name, epm) %>% spread(key = season, value = epm)
+op7 = epm7 %>% select(season, nba_id, name, oepm) %>% spread(key = season, value = oepm)
+dp7 = epm7 %>% select(season, nba_id, name, depm) %>% spread(key = season, value = depm)
+
+tp7 = tp7 %>% filter(`2015` != `2016` & !is.na(`2015`) & !is.na(`2016`))
+ep7 = ep7 %>% filter(name %in% tp7$name)
+op7 = op7 %>% filter(name %in% tp7$name)
+dp7 = dp7 %>% filter(name %in% tp7$name)
+
+paths7 = tp7 %>%
+  left_join(ep7, by = c("nba_id", "name"), suffix = c("", ".e")) %>%
+  left_join(op7, by = c("nba_id", "name"), suffix = c("", ".o")) %>%
+  left_join(dp7, by = c("nba_id", "name"), suffix = c("", ".d"))
+
+paths7 = paths7 %>% mutate(old.yr = 2015, new.yr = 2016) %>%
+  rename(oldtm = `2015`, newtm = `2016`, old.epm = `2015.e`, new.epm = `2016.e`,
+         old.oepm = `2015.o`, new.oepm = `2016.o`, old.depm = `2015.d`, new.depm = `2016.d`)
+
+# repeat this process for the next set of years
+epm8 = rbind(epm15, epm14)
+tp8 = epm8 %>% select(season, nba_id, name, team) %>% spread(key = season, value = team)
+ep8 = epm8 %>% select(season, nba_id, name, epm) %>% spread(key = season, value = epm)
+op8 = epm8 %>% select(season, nba_id, name, oepm) %>% spread(key = season, value = oepm)
+dp8 = epm8 %>% select(season, nba_id, name, depm) %>% spread(key = season, value = depm)
+
+tp8 = tp8 %>% filter(`2014` != `2015` & !is.na(`2014`) & !is.na(`2015`))
+ep8 = ep8 %>% filter(name %in% tp8$name)
+op8 = op8 %>% filter(name %in% tp8$name)
+dp8 = dp8 %>% filter(name %in% tp8$name)
+
+paths8 = tp8 %>%
+  left_join(ep8, by = c("nba_id", "name"), suffix = c("", ".e")) %>%
+  left_join(op8, by = c("nba_id", "name"), suffix = c("", ".o")) %>%
+  left_join(dp8, by = c("nba_id", "name"), suffix = c("", ".d"))
+
+paths8 = paths8 %>% mutate(old.yr = 2014, new.yr = 2015) %>%
+  rename(oldtm = `2014`, newtm = `2015`, old.epm = `2014.e`, new.epm = `2015.e`,
+         old.oepm = `2014.o`, new.oepm = `2015.o`, old.depm = `2014.d`, new.depm = `2015.d`)
+
+# combine all path data sets
+df = rbind(paths1, paths2, paths3, paths4, paths5, paths6, paths7, paths8)
+# check that the final data is clean
+df %>% is.na %>% sum
+
+# DATA MUNGING COMPLETE
+
+# ANALYSIS STAGE:
+# adjust for age? would be really difficult to collect data for that
 # compare offensive vs defensive EPMs
 # compare team effect on negative vs positive players
-
-
